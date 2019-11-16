a3fd5990dd7c6ffb712a6ad40a4ac166742d09dd
Ubuntu <ubuntu2@ubuntu2.xajghiw2iiaencmmebf3dvnnvh.zx.internal.cloudapp.net>
2019-11-16 23:52:21 +0000

    Tweak extractor :sun:

--- extractor.py
+++ extractor.py
@@ -15,11 +15,14 @@ def auth():
 
 def geo_get_users(api, n, geocode="40.68908,-73.95860,200km", lang="en", date_since="2018-10-15"):
     users_names = set()
-    while len(users_names) <= n:
-        for tweet in tw.Cursor(
-                api.search,
-                geocode=geocode,
-                lang=lang,
-                since=date_since).items(10):
-            users_names.add(tweet.user.screen_name)
+    for tweet in tw.Cursor(
+        api.search,
+        geocode=geocode,
+        lang=lang,
+        since=date_since
+    ).items(1.2*n):
+        users_names.add(tweet.user.screen_name)
+        if len(users_names) >= n:
+            break
+
     return list(users_names)
