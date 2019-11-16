db039821f31da1d49ea6786a357a1f1b7f01c284
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2019-11-16 19:34:03 +0000

    fixes to account crawler

--- crawler_helper.py
+++ crawler_helper.py
@@ -4,27 +4,23 @@ import time
 
 ### put api here
 
-api = None
 
 
-def get_mentions_of_user(starting_account):
+def get_mentions_of_user(starting_account, api):
     db = {starting_account : None}
     accounts_to_crawl = [starting_account]
     acc_counter = 0
     for account in accounts_to_crawl:
-        print(f"{acc_counter}: {account}")
         account_mentions = []
         acc_counter += 1
-        if acc_counter > 100:
+        if acc_counter > 11:
             break
         try:
             for status in Cursor(api.user_timeline, id=account).items(100):
                 if "user_mentions" in status.entities:
                     for mention in status.entities["user_mentions"]:
                         account_mentions.append(mention["screen_name"])
-            print(len(account_mentions))
             account_mentions = pd.Series(account_mentions).value_counts()
-            print(account_mentions)
             account_mentions = list(account_mentions.index)
             db[account] = account_mentions
             accounts_to_crawl.extend(account_mentions[:10])
@@ -32,6 +28,4 @@ def get_mentions_of_user(starting_account):
             time.sleep(5)
             print('boom')
             continue
-    for k in db:
-        print(f"{k}: {db[k]}")
-    print(pd.Series(data=accounts_to_crawl).value_counts())
+    return db
