9570871a4e2a7c6f377bf339b01e3035f53b6387
Apolexian <ivananikitin98@gmail.com>
2019-11-16 16:18:36 +0000

    extractor works locally, not on azure

--- extractor.py
+++ extractor.py
@@ -9,10 +9,35 @@ auth.set_access_token(credentials["access_token"], credentials["access_secret"])
 
 api = tw.API(auth, wait_on_rate_limit=True)
 date_since = "2018-10-15"
-tweets = tw.Cursor(api.search,
-                   geocode="43.17305,-77.62479,120km",
-                   lang="en",
-                   since=date_since).items(5)
+users_names = set()
+for tweet in tw.Cursor(api.search,
+                       geocode="40.68908,-73.95860,200km",
+                       lang="en",
+                       since=date_since).items(300):
+    print(tweet)
+    users_names.add(tweet.user.screen_name)
 
-for tweet in tweets:
-    print(tweet.created_at, tweet.text, tweet.lang)
+user_friends = {}
+user_tweets = {}
+for user in users_names:
+    print(user)
+    for tweet in tw.Cursor(api.user_timeline, screen_name=user).items(100):
+        if user in user_tweets.keys():
+            user_tweets[user].append(tweet.text)
+        else:
+            user_tweets[user] = [tweet.text]
+
+with open('user_tweets.json', 'w') as f:
+    print("dumped")
+    json.dump(user_tweets, f, ensure_ascii=False, indent=4)
+
+for user in users_names:
+    for friend in tw.Cursor(api.friends, screen_name=user).items(100):
+        if user in user_friends.keys():
+            user_friends[user].append(friend.screen_name)
+        else:
+            user_friends[user] = [friend.screen_name]
+
+with open('user_friends.json', 'w') as f:
+    print("dumped friends")
+    json.dump(user_friends, f, ensure_ascii=False, indent=4)
