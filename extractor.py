2691b5ae1f2661d9d1772d3370f32e9228645eda
Apolexian <ivananikitin98@gmail.com>
2019-11-16 19:08:14 +0000

    tweepy scrapes only for tweets now and also has entities

--- extractor.py
+++ extractor.py
@@ -13,31 +13,20 @@ users_names = set()
 for tweet in tw.Cursor(api.search,
                        geocode="40.68908,-73.95860,200km",
                        lang="en",
-                       since=date_since).items(300):
-    print(tweet)
+                       since=date_since).items(100):
     users_names.add(tweet.user.screen_name)
+    print("got user: " + str(tweet.user.screen_name))
 
-user_friends = {}
 user_tweets = {}
 for user in users_names:
-    print(user)
     for tweet in tw.Cursor(api.user_timeline, screen_name=user).items(100):
+        print("extracting tweet for: " + str(user))
         if user in user_tweets.keys():
-            user_tweets[user].append(tweet.text)
+            user_tweets[user].append((tweet.text, tweet.entities))
         else:
-            user_tweets[user] = [tweet.text]
+            user_tweets[user] = [(tweet.text, tweet.entities)]
 
-with open('user_tweets.json', 'w') as f:
+with open('user_tweets.json', 'a') as f:
     print("dumped")
     json.dump(user_tweets, f, ensure_ascii=False, indent=4)
 
-for user in users_names:
-    for friend in tw.Cursor(api.friends, screen_name=user).items(100):
-        if user in user_friends.keys():
-            user_friends[user].append(friend.screen_name)
-        else:
-            user_friends[user] = [friend.screen_name]
-
-with open('user_friends.json', 'w') as f:
-    print("dumped friends")
-    json.dump(user_friends, f, ensure_ascii=False, indent=4)
