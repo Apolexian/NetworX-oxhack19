e86b872135cf8c390924d88b5c9ba35436ef3ad7
David <dmartinezmillan@hotmail.com>
2019-11-17 01:55:32 +0000

    Changed for the new version of data 2

--- graph_creation.py
+++ graph_creation.py
@@ -12,11 +12,6 @@ def add_user(tx, name, friend_name):
            "MERGE (u2:User {name: friend}) "
            "CREATE (u1)<-[:METION]-(u2) ",
            name=name, friend=friend_name)
-'''
-with driver.session() as session:
-    for user, friends_list in users.items():
-        session.write_transaction(add_user, user, friends_list)
-'''
 
 def main():
     files = os.listdir(dir)
