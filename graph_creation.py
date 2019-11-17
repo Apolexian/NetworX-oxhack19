04ebbdc03faedf7b52de9370915be6fd744f2e9d
David <dmartinezmillan@hotmail.com>
2019-11-17 01:55:18 +0000

    Changed for the new version of data

--- graph_creation.py
+++ graph_creation.py
@@ -1,7 +1,9 @@
-from dump_file import users
 from neo4j import GraphDatabase
+import json
+import os
 
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "neo4j"))
+dir = './scrape'
 
 def add_user(tx, name, friend_name):
     tx.run("MERGE (u1:User {name: $name}) "
@@ -10,8 +12,21 @@ def add_user(tx, name, friend_name):
            "MERGE (u2:User {name: friend}) "
            "CREATE (u1)<-[:METION]-(u2) ",
            name=name, friend=friend_name)
-
+'''
 with driver.session() as session:
     for user, friends_list in users.items():
         session.write_transaction(add_user, user, friends_list)
+'''
 
+def main():
+    files = os.listdir(dir)
+    for file in files:
+        with open(dir + '/' + str(file), 'r') as f:
+            datastore = json.load(f)
+            with driver.session() as session:
+                for user, friends_list in datastore.items():
+                    print(user)
+                    session.write_transaction(add_user, user, friends_list)
+
+if __name__ == "__main__":
+    main()
