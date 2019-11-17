78d43940515d538217ecdb40f76bbee559554e30
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2019-11-17 03:41:33 +0000

    prepped centrality and pol engagement

--- centrality_helper.py
+++ centrality_helper.py
@@ -1,18 +1,8 @@
 import networkx as nx
-import json
-
-def get_example_network():
-    json1_file = open('example_network.json', 'r')
-    json1_str = json1_file.read()
-    json1_file.close()
-    json1_data = json.loads(json1_str)
-    return json1_data
+import pandas as pd
 
 
-def get_network_degree_centrality(network_dict):
-    G = nx.DiGraph()
-    G.add_nodes_from(network_dict.keys())
-    for k, v in network_dict.items():
-        G.add_edges_from(([(k, t) for t in v]))
-    return nx.degree_centrality(G)
+def get_network_degree_centrality(G):
+    return pd.Series(nx.degree_centrality(G)).sort_values(ascending=False)
+
 
--- political_eng_converter.py
+++ political_eng_converter.py
@@ -1,4 +1,5 @@
 import string
+from tweepy import Cursor, TweepError
 
 TWEET_LIMIT = 100
 
@@ -14,20 +15,21 @@ def intersection(lst1, lst2):
     return list(set(lst1) & set(lst2))
 
 
-def assign_political_engagement(dictionary):
+def extract_account_engagement(accounts, api):
     political_terms = get_political_terms()
-    political_engagement = {}
-    for user, tweets in dictionary.items():
+    engagement_output = {}
+    for account in accounts:
         terms_count = 0
-        tweet_count = 0
-        for tweet in tweets:
-            table = str.maketrans(dict.fromkeys(string.punctuation))
-            content = tweet.translate(table).split()
-            common_terms = intersection(political_terms, content)
-            tweet_count += 1
-            terms_count += len(common_terms)
-        if tweet_count != 0:
-            political_engagement[user] = terms_count / tweet_count
-        else:
-            political_engagement[user] = 0
-    return political_engagement
+        try:
+            for status in Cursor(api.user_timeline, id=account).items(TWEET_LIMIT):
+                table = str.maketrans(dict.fromkeys(string.punctuation))
+                content = status.text.translate(table).split()
+                common_terms = intersection(political_terms, content)
+                terms_count += len(common_terms)
+            engagement_output[account] = terms_count/TWEET_LIMIT
+        except TweepError:
+            continue
+    return engagement_output
+
+
+
